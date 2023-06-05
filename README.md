29f5ea12f0a8b084937274b9ca3160b41630168a
Andres-ramos <ramosandres443@gmail.com>
2023-06-04 23:47:29 -0400

    Updates to rffs api, notebook and README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rrfs
+
+The project consists of a notebook 
+
+To run the notebook, create the enviroment from the enviroment.yml file using conda. Run the command
+
+conda env create -f environment.yml
+
--- RFFS/rffs.py
+++ RFFS/rffs.py
@@ -1,30 +1,34 @@
 import os
 import xarray as xr
 
+#Receive variable from somewhere else
+cache_path="./model_output"
+
+error_message = "Model file missing from cache. Please download model file"
+
 def make_model_file_name(initialization_hour, forecast_hour):
-    print(initialization_hour)
     file_name = f'rrfs.t{initialization_hour}z.natlev.f00{forecast_hour}.conus_3km.grib2'
     return file_name
 
-def get_file_from_store(date_time, initialization_hour, forecast_hour):
-    init_hour_str = str(initializatioxn_hour)
+def fetch_file(date_time, initialization_hour, forecast_hour):
+    init_hour_str = str(initialization_hour)
     f_hour_str = str(forecast_hour)
     #TODO: Figure out how to convert this to string
     date_time_str = date_time
     #Checks if file is in date time file folder
-    if date_time_str in os.listdir("../model_output"):
+    if date_time_str in os.listdir(cache_path):
         #Check if folder for model date exists
-        if init_hour_str in os.listdir(f"../model_output/{date_time_str}"):
+        if init_hour_str in os.listdir(f"{cache_path}/{date_time_str}"):
             file_name = make_model_file_name(initialization_hour,forecast_hour)
-            if file_name in os.listdir(f"../model_output/{date_time_str}/{init_hour_str}"):
+            if file_name in os.listdir(f"{cache_path}/{date_time_str}/{init_hour_str}"):
                 #If file has been downloaded, return dataset
-                return xr.open_dataset(f"../model_output/{date_time_str}/{init_hour_str}/{file_name}", engine='pynio')
+                return xr.open_dataset(f"{cache_path}/{date_time_str}/{init_hour_str}/{file_name}", engine='pynio')
             else :
                 #TODO: 
                 #Forecast time has not yet been used
                 #Fetch file
                 #return xarray dataset
-                raise Exception("Download model file")
+                raise Exception(error_message)
                 return 
         else :
             #TODO:
@@ -32,7 +36,7 @@ def get_file_from_store(date_time, initialization_hour, forecast_hour):
             #Create folder for initialization hour 
             #Fetch file from s3 bucket and put in folder
             #Return xarray dataset 
-            raise Exception("Download model file")
+            raise Exception(error_message)
             return 
     #If file is not in date time file folder 
     else :
@@ -42,7 +46,7 @@ def get_file_from_store(date_time, initialization_hour, forecast_hour):
         #Create folder for initialization hour
         #Fetch file from s3 bucket and put in folder 
         #Return xarray dataset
-        raise Exception("Download model file")
+        raise Exception(error_message)
         return
             
     return 
@@ -51,7 +55,7 @@ def get_file_from_store(date_time, initialization_hour, forecast_hour):
 #TODO: Implement
 def create_folder_structure(level, date_time, ):
     if level == 'd':
-        os.makedir(f"../model_output/{date_time}")
+        os.makedir(f"{cache_path}/{date_time}")
 
 #TODO: Implement once credentials are figured out
 def fetch_model_data(file_name):
