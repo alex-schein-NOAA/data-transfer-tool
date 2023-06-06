e1cb9de6db65b27cc3a793451488e501dafaffd3
Andres-ramos <ramosandres443@gmail.com>
2023-06-05 21:41:25 -0400

    Added S3 connector class.

---
+++ RFFS/s3/s3.py
@@ -0,0 +1,22 @@
+import boto3
+class S3Connector:
+    def __init__(self, bucket_name):
+        self.bucket_name = bucket_name
+        self.s3 = boto3.client('s3',region_name='us-east-1')
+    
+    #Fetches file from bucket
+    def fetch_file(self, date_time_string, init_hour_string, file_name):
+        try :
+
+            object_name = self.create_object_name(date_time_string, init_hour_string, file_name)
+            with open(file_name, 'wb') as f:
+                self.s3.download_fileobj(self.bucket_name, object_name,f)
+               
+        except :
+            raise Exception(f"File download Error: Failed to download file {object_name} from bucket {self.bucket_name}")
+
+    #Helper function
+    #Creates object name for file in bucket
+    def create_object_name(self, date_time_string, init_hour_string, file_name):
+        return f"rrfs_a/rrfs_a.{date_time_string}/{init_hour_string}/control/{file_name}"
+    
