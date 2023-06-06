34f04d937053ce2aeddce6a916495d8076063c01
Andres-ramos <ramosandres443@gmail.com>
2023-06-05 21:34:52 -0400

    Added cache class.

---
+++ RFFS/cache/cache.py
@@ -0,0 +1,21 @@
+import os
+
+class Cache:
+    def __init__(self, cache_name):
+        self.evaluate_cache(cache_name)
+        return 
+    
+    #Evaluates if cache exists. Otherwise it creates it
+    #In the future, can send memory usage information object
+    def evaluate_cache(self, cache_name):
+        if os.path.exists(cache_name):
+            return 
+        else :
+            return 
+
+    #TODO: Implement!
+    def create_cache(self, cache_name):
+        #Make directory named "cache_name"
+        #Create directories with dates
+        #Create directories with hours
+        return 
